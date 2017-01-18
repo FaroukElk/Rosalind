4ce713b175e05aa6f7f2e606f3131cd38f657da8
FaroukElk <faroukelkhayat@outlook.com>
2017-01-17 21:46:45 -0500

    Solution to "Computing GC Content"

--- GC_Content/GC_content.py
+++ GC_Content/GC_content.py
@@ -1,5 +1,18 @@
 from sys import argv
-script, filename = argv
+
+#Iterates through sequence dictionary and finds the sequence with the highest GC content
+def gc_content(sequences):
+	GC_content = {}
+	max_ID = ""
+	max_percent = 0
+	for ID, DNA in sequences.items():
+		GC_count = DNA.count("G") + DNA.count("C")
+		GC_percent = float(GC_count)/len(DNA) * 100
+		GC_content[ID] = GC_percent
+		if GC_percent > max_percent:
+			max_percent = GC_percent
+			max_ID = ID
+	return GC_content.items(), max_ID, max_percent
 
 #Reads FASTA file and returns a dictionary with ID:sequence pairs
 def load_sequences(FASTA_file):
@@ -21,21 +34,9 @@ def load_sequences(FASTA_file):
 		sequences[sequence_id] = sequence
 		return sequences
 
-#Iterates through sequence dictionary and finds the sequence with the highest GC content
-def gc_content(sequences):
-	GC_content = {}
-	max_ID = ""
-	max_percent = 0
-	for ID, DNA in sequences.items():
-		GC_count = DNA.count("G") + DNA.count("C")
-		GC_percent = float(GC_count)/len(DNA) * 100
-		GC_content[ID] = GC_percent
-		if GC_percent > max_percent:
-			max_percent = GC_percent
-			max_ID = ID
-	return GC_content.items(), max_ID, max_percent
 		
 if __name__ == "__main__":
+	script, filename = argv
 	sequences = load_sequences(filename)
 	max_ID, max_percent = gc_content(sequences)[1:]
 	with open("answer.txt", "w") as answer:
